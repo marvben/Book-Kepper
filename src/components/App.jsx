80d48797089d6a37a0fc52f09fe516eb263c3a9a
unknown <benluvforu@gmail.com>
2022-07-20 09:33:55 +0100

    add note.js and use map to display contents on the front end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,16 @@ import "../index.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../note.js";
 
 function App() {
 	return (
 		<div className="">
 			<Header />
-			<Note />
+			{notes.map((note) => (
+				<Note key={note.id} heading={note.title} content={note.content} />
+			))}
+
 			<Footer />
 		</div>
 	);
